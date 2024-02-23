8aecf5a4b25668fc2970bcf7c291d41723869daa
Zaki Lazuardi Ferysa Putra <zakilfp987@gmail.com>
2024-02-23 10:06:43 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+// use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,88 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// == BASIC ROUTING ==
+use Illuminate\Support\Facades\Route;
+
+Route::get('/hello', function() {
+return 'Hello World'; });
+ 
+Route::get('/world', function () {   
+return 'World'; 
+}); 
+
+Route::get('/pesan', function() {
+    return 'Selamat Datang';
 });
+
+Route::get('/about', function() {
+    return 'Nama : Zaki Lazuardi Ferysa Putra 
+    NIM : 2241720101';
+});
+
+// == ROUTE PARAMETERS ==
+Route::get('/user/{name}', function($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}' , function($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+// == OPTIONAL PARAMETERS==
+// Route::get('/user/{name?}', function($name=null) {
+//     return 'Nama saya ' . $name;
+// });
+
+Route::get('/user/{name?}', function($name='John') {
+    return 'Nama saya ' . $name;
+});
+
+// == ROUTE NAME ==
+Route::get('/user/profile', function() {
+    //
+})->name('profile');
+
+// == ROUTE GROUP DAN ROUTE PREFIXES ==
+// Route::middleware(['first', 'second']) ->group(function () {
+//     Route::get('/', function () {
+//         // Uses first & second middleware..
+//     });
+// Route::get('/user/profile', function () {         
+//         // Uses first & second middleware... 
+//     }); 
+// }); 
+// Route::domain('{account}.example.com')->group(function() {
+//     Route::get('user/{id}', function ($account, $id) {
+//         //
+//     });
+// });
+
+// Route::middleware('auth')->group(function () { 
+//     Route::get('/user', [UserController::class, 'index']); 
+//     Route::get('/post', [PostController::class, 'index']); 
+//     Route::get('/event', [EventController::class, 'index']); 
+     
+// }); 
+
+// // == ROUTE PREFIXES ==
+// Route::prefix('admin')->group(function () { 
+//     Route::get('/user', [UserController::class, 'index']); 
+//     Route::get('/post', [PostController::class, 'index']); 
+//     Route::get('/event', [EventController::class, 'index']);  
+// }); 
+
+// == REDIRECT ROUTES ==
+Route::redirect('/here', '/there'); 
+
+// == VIEW ROUTES ==
+Route::view('/welcome', 'welcome'); 
+// Route::view('/welcome', 'welcome', ['name' => 'Taylor']); 
